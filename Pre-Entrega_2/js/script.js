adea30edb0620bfb53240aec11178e10066c7229
IgnaMZ <ignaciozabalegui86@outlook.es>
2023-11-21 15:28:04 -0300

    Updating cart functionalities.

--- Pre-Entrega_2/js/script.js
+++ Pre-Entrega_2/js/script.js
@@ -1,4 +1,80 @@
 const contProducts = document.getElementById('container');
+const modal = document.getElementById('modal');
+let cartItems = [];
+
+class Product {
+    constructor(image, name, price, id){
+        this.image = image;
+        this.name = name;
+        this.price = price;
+        this.id = id;
+        this.quantity = 1;
+        this.subtotal = 0;
+    }
+    obtenerTotal() {
+        this.subtotal = this.price * this.quantity;
+    }
+}
+
+triggerEvents();
+
+function triggerEvents() {
+    document.addEventListener('DOMContentLoaded', () => {
+        renderProducts();
+    });
+
+    contProducts.addEventListener('click', addItem);
+}
+
+function addItem(item) {
+    //console.log('Se hizo click en la aplicación');
+    item.preventDefault();
+    if (item.target.classList.contains('add-to-cart')) {
+        const addedItem = item.target.parentElement;
+        //console.log(addedItem);
+        showItemInfo(addedItem);
+    }
+}
+
+function showItemInfo(product) {
+    //console.log(product);
+    const productInfo = new Product(
+        product.querySelector('img').src, product.querySelector('h4').textContent, Number(product.querySelector('p').textContent.replace('$', '')), parseInt(product.querySelector('a').getAttribute('id')));
+
+        productInfo.obtenerTotal()
+        //console.log(productInfo);
+
+        addToCart(productInfo);
+}
+
+function addToCart(addItem) {
+    // console.log(addItem);
+    // console.log(cartItems);
+
+    const inCart = cartItems.some((item) => item.id === addItem.id)
+
+    if (inCart) {
+        // Creamos un nuevo array con los productos del carrito actualizado.
+        const products = cartItems.map((item) => {
+            if (item.id === addItem.id) {
+                item.quantity++;
+                item.subtotal = item.price * item.quantity;
+                // Retornamos el objeto item (cantidad y subtotal actualizados)
+                return item;
+            } else {
+                // Retornamos el producto que estaba en el carrito, pero sin actualizar ya que no coincide con el nuevo producto agregado.
+                return item;
+            }
+        })
+        // Reasignamos con el arrat de productos devuelto por el método.
+        cartItems = products;
+    } else {
+        // Agregamos un nuevo producto al carrito
+        cartItems.push(addItem);
+    }
+    console.log(cartItems);
+}
+
 
 const renderProducts = () => {
     products.forEach((product) => {
@@ -14,5 +90,3 @@ const renderProducts = () => {
                         contProducts.append(itemCard);
     })  
 };
-
-renderProducts();
